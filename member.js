dedfa0599e0f641f5ba7001a87a8872b36b55974
Aditya <62930521+adeeteya@users.noreply.github.com>
2024-09-05 07:16:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        {name: 'HTML5', score: 9},
+        {name: 'CSS3', score: 8},
+        {name: 'JavaScript', score: 7},
+        {name: 'AngularJS', score: 6},
+        {name: 'Node.js', score: 5},
+        {name: 'MongoDB', score: 4}
+    ];
+}
